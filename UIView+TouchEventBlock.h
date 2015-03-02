a1f831c2e87d29abc2a83d6cef7003f62842f7b5
doluvor <doluvor@126.com>
2015-03-02 18:56:48 +0800

    Add more touch event blocks.

--- UIView+TouchEventBlock.h
+++ UIView+TouchEventBlock.h
@@ -8,11 +8,15 @@
 #import <UIKit/UIKit.h>
 
 typedef void (^TouchesBeganBlock)(NSSet *touches, UIEvent *event);
-typedef void (^TouchesOutsideBlock)(CGPoint point, UIEvent *event);
+typedef void (^TouchesMovedBlock)(NSSet *touches, UIEvent *event);
+typedef void (^TouchesEndedBlock)(NSSet *touches, UIEvent *event);
+typedef void (^TouchesCancelledBlock)(NSSet *touches, UIEvent *event);
 
 @interface UIView (TouchEventBlock)
 
 @property (nonatomic, copy) TouchesBeganBlock touchesBeganBlock;
-@property (nonatomic, copy) TouchesOutsideBlock touchesOutsideBlock;
+@property (nonatomic, copy) TouchesMovedBlock touchesMovedBlock;
+@property (nonatomic, copy) TouchesEndedBlock touchesEndedBlock;
+@property (nonatomic, copy) TouchesCancelledBlock touchesCancelledBlock;
 
 @end
--- UIView+TouchEventBlock.m
+++ UIView+TouchEventBlock.m
@@ -9,12 +9,16 @@
 #import <objc/runtime.h>
 
 static const void *TouchesBeganBlockKey = &TouchesBeganBlockKey;
-static const void *TouchesOutsideBlockKey = &TouchesOutsideBlockKey;
+static const void *TouchesMovedBlockKey = &TouchesMovedBlockKey;
+static const void *TouchesEndedBlockKey = &TouchesEndedBlockKey;
+static const void *TouchesCancelledBlockKey = &TouchesCancelledBlockKey;
 
 @implementation UIView (TouchEventBlock)
 
 @dynamic touchesBeganBlock;
-@dynamic touchesOutsideBlock;
+@dynamic touchesMovedBlock;
+@dynamic touchesEndedBlock;
+@dynamic touchesCancelledBlock;
 
 - (void)setTouchesBeganBlock:(TouchesBeganBlock)touchesBeganBlock {
     objc_setAssociatedObject(self, TouchesBeganBlockKey, touchesBeganBlock, OBJC_ASSOCIATION_COPY);
@@ -24,18 +28,61 @@ static const void *TouchesOutsideBlockKey = &TouchesOutsideBlockKey;
     return objc_getAssociatedObject(self, TouchesBeganBlockKey);
 }
 
-- (void)setTouchesOutsideBlock:(TouchesOutsideBlock)touchesOutsideBlock {
-    objc_setAssociatedObject(self, TouchesOutsideBlockKey, touchesOutsideBlock, OBJC_ASSOCIATION_COPY);
+- (void)setTouchesMovedBlock:(TouchesMovedBlock)touchesMovedBlock {
+    objc_setAssociatedObject(self, TouchesMovedBlockKey, touchesMovedBlock, OBJC_ASSOCIATION_COPY);
 }
 
-- (TouchesOutsideBlock)touchesOutsideBlock {
-    return objc_getAssociatedObject(self, TouchesOutsideBlockKey);
+- (TouchesMovedBlock)touchesMovedBlock {
+    return objc_getAssociatedObject(self, TouchesMovedBlockKey);
+}
+
+- (void)setTouchesEndedBlock:(TouchesEndedBlock)touchesEndedBlock {
+    objc_setAssociatedObject(self, TouchesEndedBlockKey, touchesEndedBlock, OBJC_ASSOCIATION_COPY);
+}
+
+- (TouchesEndedBlock)touchesEndedBlock {
+    return objc_getAssociatedObject(self, TouchesEndedBlockKey);
+}
+
+- (void)setTouchesCancelledBlock:(TouchesCancelledBlock)touchesCancelledBlock {
+    objc_setAssociatedObject(self, TouchesCancelledBlockKey, touchesCancelledBlock, OBJC_ASSOCIATION_COPY);
+}
+
+- (TouchesCancelledBlock)touchesCancelledBlock {
+    return objc_getAssociatedObject(self, TouchesCancelledBlockKey);
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     [super touchesBegan:touches withEvent:event];
     
     TouchesBeganBlock block = self.touchesBeganBlock;
+    
+    if (block != NULL)
+        block(touches, event);
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesMoved:touches withEvent:event];
+    
+    TouchesMovedBlock block = self.touchesMovedBlock;
+    
+    if (block != NULL)
+        block(touches, event);
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesEnded:touches withEvent:event];
+    
+    TouchesEndedBlock block = self.touchesEndedBlock;
+    
+    if (block != NULL)
+        block(touches, event);
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesCancelled:touches withEvent:event];
+    
+    TouchesCancelledBlock block = self.touchesCancelledBlock;
     
     if (block != NULL)
         block(touches, event);
